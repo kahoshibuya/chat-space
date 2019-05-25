cecd9a0638aafa134297317aa095e427fe8074d8
kahoshibuya <a14140.ks@gmail.com>
2019-05-26 01:03:58 +0900

    update

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,10 +2,10 @@ $(function() {
 var search_list = $(".chat-group-form__input");
 
 function appendUser(user) {
-  var html = `<div class="chat-group-user clearfix">
-                <input name = 'group[user_ids][]' type='hidden' value=${id}>
+  var html = `<div class='chat-group-user clearfix js-chat-member' id ='chat-group-users'>
+                <input name='group[user_ids][]' type='hieen' value=${id}>
                 <p class="chat-group-user__name">${name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${id}" data-user-name="${name}">追加</div>
               </div>`
   // search_list.append(html);
   return html;
@@ -29,6 +29,7 @@ function appendUser(user) {
         users.forEach(function(users){
           var html = appendUser(user);
           result.append(html)
+          $('.users').append(html);
         })
       }
     })
