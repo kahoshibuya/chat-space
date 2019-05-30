c348a937dce37a7f28a0f5979b5f0e90018ca8fe
kahoshibuya <a14140.ks@gmail.com>
2019-05-30 23:02:26 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,18 +1,18 @@
 $(function() {
-    function buildHTML(message) {
-      var imgHTML = (message.image == null)
-                  ?("")
-                  :(`<image class="lower-message__image" src="${message.image}">`);
+  function buildHTML(message) {
+    var imgHTML = (message.image == null)
+                ?("")
+                :(`<image class="lower-message__image" src="${message.image}">`);
 
-      var new_message = `<div class="message">
-              <div class="upper-info">
-                <div class="upper-info__user">${message.name} </div>
-                <div class="upper-info__date">${message.time} </div>
-              </div>
-              <div class="message__text">
-                <p class ="message__text__content">${message.content}</p> 
-                ${imgHTML}
-              </div>`
+    var new_message = `<div class="message">
+            <div class="upper-info">
+              <div class="upper-info__user">${message.name} </div>
+              <div class="upper-info__date">${message.time} </div>
+            </div>
+            <div class="message__text">
+              <p class ="message__text__content">${message.content}</p> 
+              ${imgHTML}
+            </div>`
     return new_message;
   }
 
@@ -39,38 +39,38 @@ $(function() {
     })
     .fail(function(){
       // ajaxのリクエストが失敗
+      alert('メッセージを送信できません');
     })
     .always(function() {
       $('.submit-btn').prop('disabled', false);
     })
   });
 
-  $(function(){
-    $(function(){
-      setInterval(update, 10000);
+  var reloadMessages = (function(){
+    if (location.href.match(/\/groups\/\d+\/messages/)){
+      $('.timeline__body').animate({scrollTop: $('.timeline__body')[0].scrollHeight}, 'fast');
+      last_message_id = $('.messages:last').data('id');
+    $.ajax({
+      url: location.href,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+
+    .done(function(messages) {
+      var insertHTML = buildHTML(message)
+      data.forEach(function(messages) {
+        var html = buildHTML(message)
+        append(html)
+        $('.messages-box').animate({scrollTop: $('.messages-box')[0].scrollHeight});
+      })
+      console.log('success');
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
     });
-    function update(){
-        var message_id = $('.messages:last').data('id');
-      $.ajax({
-        //ルーティングで設定した通りのURLを指定
-        url: location.href,
-        //ルーティングで設定した通りhttpメソッドをgetに指定
-        type: 'get',
-        dataType: 'json',
-        //dataオプションでリクエストに値を含める
-        data: {id: message_id}
-      })
-      .always(function(data){ //通信したら、成功しようがしまいが受け取ったデータ（@new_message)を引数にとって以下のことを行う
-        $.each(data, function(i, data){ //'data'を'data'に代入してeachで回す
-          buildMESSAGE(data); //buildMESSAGEを呼び出す
-        });
-      });
-      .done(function(messages) {
-        console.log('success');
-      })
-      .fail(function() {
-        console.log('error');
-      });
-    };
-  })
+    } else {
+      clearInterval(interval);
+    }
+  } , 5000 );
 })
